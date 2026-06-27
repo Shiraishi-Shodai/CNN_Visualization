053778253414c95a415ae0fc1db46c433f8c5658
Shoda-Shiraishi <siranosuke1227@gmail.com>
2026-06-28 05:09:50 +0900

    VGG16 preview

--- src/models/vgg16.py
+++ src/models/vgg16.py
@@ -2,6 +2,7 @@ from sequential import Sequential
 from model_builder import ModelBuilder
 from utils import load_yaml
 from pathlib import Path
+import torch
 
 yaml_file = Path(r"config/VGG16.yaml")
 config = load_yaml(yaml_file)
@@ -11,3 +12,13 @@ layers = mb.build()
 
 for layer in layers:
     print(layer)
+
+print(len(layers))
+
+model = Sequential(layers)
+print(model)
+
+x = torch.randn((2, 3, 224, 224))
+out = model.forward(x)
+
+print(out.shape)
