faedc91822e20e520dd808335245b70e076c36ed
Shoda-Shiraishi <siranosuke1227@gmail.com>
2026-06-28 00:11:36 +0900

    initializerを追加2

--- src/initializers.py
+++ src/initializers.py
@@ -13,19 +13,27 @@ def register(name):
 
 
 @register("he")
-def he_initializer(fan_in, fan_out, shape):
+def he_initializer(shape, fan_in, fan_out=None):
     return nn.Parameter(torch.tensor(np.random.normal(
         loc=0,
         scale=np.sqrt(2/fan_in),
         size=shape
     ).astype("float")), requires_grad=False)
     
-
 @register("xavier")
-def he_initializer(fan_in, fan_out, shape):
+def xavier_initializer(shape, fan_in, fan_out):
     return nn.Parameter(torch.tensor(np.random.normal(
         loc=0,
         scale=np.sqrt(2/(fan_in + fan_out)),
         size=shape
     ).astype("float")), requires_grad=False)
     
+@register("zeros")
+def zeros_initializer(shape, fan_in=None, fan_out=None):
+    return nn.Parameter(torch.tensor(np.zeros(shape).astype("float")), requires_grad=False)
+
+def get_initializer(name):
+    if name not in INITIALIZERS:
+        raise ValueError(f"{name}は登録されていません")
+    
+    return INITIALIZERS[name]
